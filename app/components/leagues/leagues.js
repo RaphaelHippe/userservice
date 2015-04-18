6cf458d256d275cf15ac69429d6155be8fb336d7
Benni <Benjamin.Kurczyk@gmail.com>
2015-04-18 19:36:06 +0200

    new service for riot API, refactor on service structure, template directive for player data

--- app/components/leagues/leagues.js
+++ app/components/leagues/leagues.js
@@ -1,24 +1,13 @@
 var module = angular.module('leagueModule', ['ui.select', 'ngSanitize']);
 
-module.controller('LeaguesCtrl', function ($scope, $http, ChampionImageService) {
+module.controller('LeaguesCtrl', function ($scope, $http, ChampionImageService, GameService) {
+  $('#spinner').hide();
+  $('#playerTitle').hide();
   var sumId;
   var data;
   var champions = [];
-  $scope.gameData = {team1: {
-                      players:{ player1:{},
-                                player2:{},
-                                player3:{},
-                                player4:{},
-                                player5:{}}
-                      },
-                     team2: {
-                      players:{ player1:{},
-                                player2:{},
-                                player3:{},
-                                player4:{},
-                                player5:{}}
-                      }
-                    };
+  $scope.loading;
+  $scope.gameData = {};
   $scope.players = ["OberstK", "Mindmesser"];
   $scope.roles = [{name: "Top"},
                   {name: "Jungle"} ,
@@ -26,9 +15,20 @@ module.controller('LeaguesCtrl', function ($scope, $http, ChampionImageService) 
                   {name: "ADC"},
                   {name: "Support"}
                   ];
+  $scope.player;
 
   $scope.search = function(player){
-    $http.get('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/'+player+'?api_key=466e64cb-39cc-4832-afc4-f4c1cc017533').then(function(riotResponse) {
+    $('#playerTitle').show();
+    $scope.player = player;
+    $('#spinner').show();
+    GameService.getGamesForPlayer(player).then(function(data){
+      $scope.gameData = data;
+      $scope.loading = false;
+      console.log($scope.gameData);
+      $('#spinner').hide();
+    });
+
+    /*$http.get('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/'+player+'?api_key=466e64cb-39cc-4832-afc4-f4c1cc017533').then(function(riotResponse) {
       sumId = riotResponse.data[player.toLowerCase()].id;
     }).then(function() {
       console.log(sumId);
@@ -53,7 +53,7 @@ module.controller('LeaguesCtrl', function ($scope, $http, ChampionImageService) 
         $scope.matches = data;
         console.log(data);
       });
-    });
+    });*/
   }
 
   $scope.timeConvert = function(time){
@@ -66,17 +66,57 @@ module.controller('LeaguesCtrl', function ($scope, $http, ChampionImageService) 
   }
 });
 
-module.factory("GameService", function(){
+module.factory("GameService", function($http, $q, ChampionImageService){
+  var sumId;
+  var data;
+  var gameData;
+  var promises;
   var gameService = {
-
+    getGamesForPlayer: function(playerName){
+      gameData = [];
+      promises = [];
+      var promise = $http.get('https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/'+playerName+'?api_key=466e64cb-39cc-4832-afc4-f4c1cc017533', { cache: true}).then(function(riotResponse) {
+        sumId = riotResponse.data[playerName.toLowerCase()].id;
+      }).then(function() {
+        var promise = $http.get('https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/'+sumId+'/recent?api_key=466e64cb-39cc-4832-afc4-f4c1cc017533', { cache: true}).then(function(riotResponse) {
+          data = riotResponse.data["games"];
+          angular.forEach(data, function(game){
+            var gameObj = {team1:{},
+                            team2:{}};
+            var players = game.fellowPlayers;
+            players.push({"summonerId": sumId, "teamId": game.teamId, "championId": game.championId});
+            var teamPlayerObjects = resolvePlayersIntoTeams(players);
+            gameObj.team1 = teamPlayerObjects[0];
+            gameObj.team2 = teamPlayerObjects[1];
+            gameObj["time"] = game.createDate;
+            angular.forEach(gameObj, function(team){
+              angular.forEach(team, function(player){
+                promises.push(ChampionImageService.getImage(player.championId).then(function(data) {
+                  player["image"] = data;
+                }));
+              });
+            });
+            gameData.push(gameObj);
+          });
+          var promise = $q.all(promises).then(function(){
+            console.log(gameData);
+            return gameData;
+          });
+          return promise
+        });
+        return promise;
+      });
+      return promise;
+    }
   };
   return gameService;
 });
 
+
 module.factory("ChampionImageService", function($http){
   var champImgService = {
     getImage: function(cId){
-      var promise = $http.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+cId+"?champData=image&api_key=466e64cb-39cc-4832-afc4-f4c1cc017533").then(function(riotResponse) {
+      var promise = $http.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+cId+"?champData=image&api_key=466e64cb-39cc-4832-afc4-f4c1cc017533", { cache: true}).then(function(riotResponse) {
           return riotResponse.data["image"].full;
       });
       return promise;
@@ -84,3 +124,62 @@ module.factory("ChampionImageService", function($http){
   };
   return champImgService;
 });
+
+module.directive('playerDataDir', function() {
+    return {
+        restrict: 'EA',
+        scope: false,
+        template: function(elem, attr){
+          return  '<tr>' +
+                  '<td ng-model="gameData.team'+attr.team+'.players.player'+attr.player+'.role">' +
+                  '<ui-select theme="selectize" ng-disabled="disabled">'+
+                  '<ui-select-match placeholder="Role">{{$select.selected.name}}</ui-select-match>' +
+                  '<ui-select-choices repeat="role in roles">' +
+                  '<span ng-bind-html="role.name"></span>' +
+                  '</ui-select-choices>' +
+                  '</ui-select>' +
+                  '</td>' +
+                  '<td>' +
+                  '<img src="http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/Alistar.png" style="width:50%;">' +
+                  '</td>' +
+                  '<td>' +
+                  '<input ng-model="gameData.team'+attr.team+'.players.player'+attr.player+'.name" type="text" class="form-control" aria-label="...">' +
+                  '</td>' +
+                  '<td>' +
+                  '<input ng-model="gameData.team'+attr.team+'.players.player'+attr.player+'.kills" type="text" class="form-control" aria-label="...">' +
+                  '</td>' +
+                  '<td>' +
+                  '<input ng-model="gameData.team'+attr.team+'.players.player'+attr.player+'.deaths" type="text" class="form-control" aria-label="...">' +
+                  '</td>' +
+                  '<td>' +
+                  '<input ng-model="gameData.team'+attr.team+'.players.player'+attr.player+'.assists" type="text" class="form-control" aria-label="...">' +
+                  '</td>' +
+                  '<td>' +
+                  '<input ng-model="gameData.team'+attr.team+'.players.player'+attr.player+'.CS" type="text" class="form-control" aria-label="...">' +
+                  '<td>' +
+                  '</tr>'
+        },
+        replace: true
+    };
+});
+
+
+function resolvePlayersIntoTeams(players){
+  var team1Count = 1;
+  var team2Count = 1;
+  var team1PlayerObject = {};
+  var team2PlayerObject = {};
+  var result = [];
+  angular.forEach(players, function(player){
+    if(player.teamId == "100"){
+      team1PlayerObject["player"+team1Count] = {summonerId: player.summonerId, championId: player.championId};
+      team1Count++;
+    }else if(player.teamId == "200"){
+      team2PlayerObject["player"+team2Count] = {summonerId: player.summonerId, championId: player.championId};
+      team2Count++;
+    }
+  });
+  result.push(team1PlayerObject);
+  result.push(team2PlayerObject);
+  return result;
+}
