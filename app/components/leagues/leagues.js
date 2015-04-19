c58784776363a542df51edb8f3f6b19af9465693
Benni <Benjamin.Kurczyk@gmail.com>
2015-04-20 01:31:06 +0200

    finished data bind, added stat bind, cleaned gameObj

--- app/components/leagues/leagues.js
+++ app/components/leagues/leagues.js
@@ -1,6 +1,6 @@
 var module = angular.module('leagueModule', ['ui.select', 'ngSanitize']);
 
-module.controller('LeaguesCtrl', function ($scope, $http, GameService, ChampionService, PlayerService) {
+module.controller('LeaguesCtrl', function ($scope, $http, GameService, ChampionService, PlayerService, StatsService) {
   $('#spinner').hide();
   $('#playerTitle').hide();
   var sumId;
@@ -9,12 +9,7 @@ module.controller('LeaguesCtrl', function ($scope, $http, GameService, ChampionS
   $scope.gameData;
   $scope.player;
   $scope.players = ["OberstK", "Mindmesser"];
-  $scope.roles = [{name: "Top"},
-                  {name: "Jungle"} ,
-                  {name: "Mid"},
-                  {name: "ADC"},
-                  {name: "Support"}
-                  ];
+  $scope.roles = ["Top", "Jungle", "Mid", "ADC", "Support"];
 
   ChampionService.getAllChampions().then(function(res){
     $scope.champions = res;
@@ -38,44 +33,67 @@ module.controller('LeaguesCtrl', function ($scope, $http, GameService, ChampionS
 
   $scope.print = function(){
     console.log($scope.gameData);
-    console.log($scope.champions);
   }
 
   $scope.chooseGame = function(game){
-    var playerArray = [];
-    angular.forEach(game.team1, function(player){
-      playerArray.push(player.summonerId);
-    });
-    angular.forEach(game.team2, function(player){
-      playerArray.push(player.summonerId);
-    });
-    PlayerService.getPlayerNamesByIdArray(playerArray).then(function(res){
-      angular.forEach(game.team1, function(player){
-        angular.forEach(res, function(name, id){
-          if(player.summonerId == id){
-            player.name = name;
-          }
+    var playerArray = buildAPISumIDArray(game);
+    PlayerService.getPlayerNamesByIdArray(playerArray).then(function(playersRes){
+      StatsService.getPlayerStatsForGame(game.id).then(function(statsRes){
+        angular.forEach(game.teams.team1, function(player){
+          angular.forEach(playersRes, function(name, id){
+            if(player.summonerId == id){
+              player.name = name;
+            }
+          });
+          angular.forEach(statsRes, function(item){
+            statsExtractor(player, item, 100);
+          });
         });
-      });
-      angular.forEach(game.team2, function(player){
-        angular.forEach(res, function(name, id){
-          if(player.summonerId == id){
-            player.name = name;
-          }
+        angular.forEach(game.teams.team2, function(player){
+          angular.forEach(playersRes, function(name, id){
+            if(player.summonerId == id){
+              player.name = name;
+            }
+          });
+          angular.forEach(statsRes, function(item){
+            statsExtractor(player, item, 200);
+          });
         });
+        $scope.gameData = game;
       });
-      $scope.gameData = game;
     });
   }
 });
+
+function statsExtractor(player, item, team){
+  if(player.championId == item.championId && item.teamId == team){
+    player.kills = item.stats.kills;
+    player.assists = item.stats.assists;
+    player.deaths = item.stats.deaths;
+    player.cs = item.stats.minionsKilled+item.stats.neutralMinionsKilled;
+    if(item.timeline.lane == "BOTTOM"){
+      if(item.timeline.role == "DUO_CARRY"){
+        player.role = "ADC";
+      }else if(item.timeline.role == "DUO_SUPPORT"){
+        player.role = "Support";
+      }
+    }else if(item.timeline.lane == "JUNGLE"){
+      player.role = "Jungle";
+    }else if(item.timeline.lane == "TOP"){
+      player.role = "Top";
+    }else if(item.timeline.lane == "MID" || item.timeline.lane == "MIDDLE"){
+      player.role = "Mid";
+    }
+  }
+}
 
 module.factory("ChampionService", function($http){
   var champService = {
     getAllChampions: function(){
       var promise = $http.get('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key=466e64cb-39cc-4832-afc4-f4c1cc017533', { cache: true}).then(function(riotResponse){
         var result = [];
-        angular.forEach(riotResponse.data.data, function(value, key){
-          result.push(key);
+        angular.forEach(riotResponse.data.data, function(value){
+          result.push(value.name);
         });
         result.sort();
         return result;
@@ -111,17 +129,19 @@ module.factory("GameService", function($http, $q, ChampionService){
         sumId = riotResponse.data[playerName.toLowerCase()].id;
       }).then(function() {
         var promise = $http.get('https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/'+sumId+'/recent?api_key=466e64cb-39cc-4832-afc4-f4c1cc017533', { cache: true}).then(function(riotResponse) {
-          data = riotResponse.data["games"];
+          data = riotResponse.data.games;
           angular.forEach(data, function(game){
-            var gameObj = {team1:{},
-                            team2:{}};
+            var gameObj = {teams:{team1:{},team2:{}}};
             var players = game.fellowPlayers;
             players.push({"summonerId": sumId, "teamId": game.teamId, "championId": game.championId});
             var teamPlayerObjects = resolvePlayersIntoTeams(players);
-            gameObj.team1 = teamPlayerObjects[0];
-            gameObj.team2 = teamPlayerObjects[1];
+            gameObj.teams.team1 = teamPlayerObjects[0];
+            gameObj.teams.team2 = teamPlayerObjects[1];
             gameObj["time"] = game.createDate;
-            angular.forEach(gameObj, function(team){
+            gameObj["type"] = game.gameType;
+            gameObj["subtype"] = game.subType;
+            gameObj["id"] = game.gameId;
+            angular.forEach(gameObj.teams, function(team){
               angular.forEach(team, function(player){
                 promises.push(ChampionService.getImage(player.championId).then(function(data) {
                   player["image"] = data;
@@ -162,21 +182,33 @@ module.factory("PlayerService", function($http){
   return playerService;
 });
 
+module.factory("StatsService", function($http){
+  var statsService = {
+    getPlayerStatsForGame: function(game){
+      var promise = $http.get('https://euw.api.pvp.net/api/lol/euw/v2.2/match/'+game+'?includeTimeline=false&api_key=466e64cb-39cc-4832-afc4-f4c1cc017533', { cache: true}).then(function(riotResponse){
+        return riotResponse.data.participants;
+      });
+      return promise
+    }
+  }
+  return statsService;
+});
+
 module.directive('playerDataDir', function() {
     return {
         restrict: 'EA',
         scope: false,
         template: function(elem, attr){
           return  '<tr>' +
-                  '<td style="width:10%;" ng-model="gameData.team'+attr.team+'.player'+attr.player+'.role">' +
+                  '<td style="width:15%;" ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.role">' +
                   '<ui-select theme="selectize" ng-disabled="disabled">'+
-                  '<ui-select-match placeholder="Role">{{$select.selected.name}}</ui-select-match>' +
+                  '<ui-select-match placeholder="Role">{{$select.selected}}</ui-select-match>' +
                   '<ui-select-choices repeat="role in roles">' +
-                  '<span ng-bind-html="role.name"></span>' +
+                  '<span ng-bind-html="role"></span>' +
                   '</ui-select-choices>' +
                   '</ui-select>' +
                   '</td>' +
-                  '<td style="width:15%;" ng-model="gameData.team'+attr.team+'.player'+attr.player+'.champion">' +
+                  '<td style="width:15%;" ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.champion">' +
                   '<ui-select theme="selectize" ng-disabled="disabled">'+
                   '<ui-select-match placeholder="Champion">{{$select.selected}}</ui-select-match>' +
                   '<ui-select-choices repeat="champion in champions">' +
@@ -185,26 +217,25 @@ module.directive('playerDataDir', function() {
                   '</ui-select>' +
                   '</td>' +
                   '<td>' +
-                  '<input ng-model="gameData.team'+attr.team+'.player'+attr.player+'.name" type="text" class="form-control" aria-label="...">' +
+                  '<input ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.name" type="text" class="form-control" aria-label="...">' +
                   '</td>' +
                   '<td>' +
-                  '<input ng-model="gameData.team'+attr.team+'.player'+attr.player+'.kills" type="text" class="form-control" aria-label="...">' +
+                  '<input ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.kills" type="text" class="form-control" aria-label="...">' +
                   '</td>' +
                   '<td>' +
-                  '<input ng-model="gameData.team'+attr.team+'.player'+attr.player+'.deaths" type="text" class="form-control" aria-label="...">' +
+                  '<input ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.deaths" type="text" class="form-control" aria-label="...">' +
                   '</td>' +
                   '<td>' +
-                  '<input ng-model="gameData.team'+attr.team+'.player'+attr.player+'.assists" type="text" class="form-control" aria-label="...">' +
+                  '<input ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.assists" type="text" class="form-control" aria-label="...">' +
                   '</td>' +
                   '<td>' +
-                  '<input ng-model="gameData.team'+attr.team+'.player'+attr.player+'.CS" type="text" class="form-control" aria-label="...">' +
+                  '<input ng-model="gameData.teams.team'+attr.team+'.player'+attr.player+'.cs" type="text" class="form-control" aria-label="...">' +
                   '<td>' +
                   '</tr>'
         },
         replace: true
     };
 });
-
 
 function resolvePlayersIntoTeams(players){
   var team1Count = 1;
@@ -224,4 +255,15 @@ function resolvePlayersIntoTeams(players){
   result.push(team1PlayerObject);
   result.push(team2PlayerObject);
   return result;
+}
+
+function buildAPISumIDArray(game){
+  var playerArray = [];
+  angular.forEach(game.teams.team1, function(player){
+    playerArray.push(player.summonerId);
+  });
+  angular.forEach(game.teams.team2, function(player){
+    playerArray.push(player.summonerId);
+  });
+  return playerArray;
 }
