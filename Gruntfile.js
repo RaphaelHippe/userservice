1c6392cc1d8223fc11ba2c32d9d698f82c8ba5d5
Greg Ellis <gellis@proscape.com>
2015-04-26 04:54:03 -0400

    Updated GruntFile to almost create dist correctly
    Need to figure out a few more things to fully create a build with no
    manual changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -311,6 +311,9 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+			'components/**/*',
+			'shared/**/*',
+			'assets/**/*',
             'images/{,*/}*.{webp}',
             'fonts/*'
           ]
